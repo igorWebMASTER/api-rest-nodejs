c9924c8804e419adcd8200dbe8e088fb540b7ffa
Igor Ribeiro Sobral <igorsobral@brq.com>
2023-02-20 15:29:24 -0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,22 @@
+import fastify from 'fastify'
+import { knex } from './database'
+import crypto from 'node:crypto'
+import { env } from './env'
+
+const app = fastify()
+
+app.get('/hello', async () => {
+  const transaction = await knex('transactions')
+    .select('amount', 1000)
+    .select('*')
+
+  return transaction
+})
+
+app
+  .listen({
+    port: env.PORT,
+  })
+  .then(() => {
+    console.log('HTTP server running')
+  })

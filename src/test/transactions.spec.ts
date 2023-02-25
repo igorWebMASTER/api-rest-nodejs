f95e5bcc79f2097c8023c2649d4dca2589653c22
igorWebMASTER <igor_1917@hotmail.com>
2023-02-24 22:34:17 -0300

    test: added supertest and vitest  and make the firts test

---
+++ src/test/transactions.spec.ts
@@ -0,0 +1,55 @@
+import {  beforeAll, afterAll, it, expect, describe } from 'vitest';
+import request from 'supertest';
+import { app } from '../app';
+
+describe('Transaction routes', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+  
+  afterAll(async()=> {
+    await app.close()
+  })
+
+  const makeTransactions = () => {
+    
+  }
+  
+  it('should be able to create a new transaction', async () => {
+   const response = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New transactions',
+        amount: 5000,
+        type: 'credit'
+      })
+      .expect(201)
+  })
+
+
+  it('should be able to list all transactions', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New transactions',
+        amount: 5000,
+        type: 'credit'
+      })
+
+      const cookies = createTransactionResponse.get('Set-Cookie')
+
+      const listTransactionsResponse = await request(app.server)
+        .get('/transactions')
+        .set('Cookie', cookies)
+        .expect(200)
+
+      expect(listTransactionsResponse.body.transactions).toEqual([
+        expect.objectContaining({
+          title: 'New transactions',
+          amount: 5000
+        })
+      ])
+
+  })
+})
+
